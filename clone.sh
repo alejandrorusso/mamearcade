d975f1e17d21abf30eadc24c3ade42b6a109dbbf
Alejandro Russo <russo@chalmers.se>
2022-01-07 21:31:43 +0100

    Cloning

--- clone.sh
+++ clone.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #file: clone.sh
 
+## Clone
 URL=https://github.com/alejandrorusso/mamearcade
 
 if [ ! -d ./mamearcade ]; then
@@ -14,18 +15,27 @@ cd ./mamearcade
 CURRENT=$(git rev-parse HEAD)
 REMOTE=$(git ls-remote $URL HEAD | awk '{ print $1}')
 
-if [[ $CURRENT == $REMOTE ]]; then
-    echo No need to pull
-else
+if [[ $CURRENT != $REMOTE ]]; then
     # It should never create a conflict
     git pull
     touch ./deploynow
 fi
 
-if [ -f ./deploynow ]; then
-    echo "Copying the scripts"
-else
+## Deploy
+FILES=$(find ./map/ -type f)
+
+if [ ! -f ./deploynow ]; then
     echo "Scripts already deployed"
+    echo "Nothing to do :("
+    exit 0
 fi
+
+for f in $FILES
+do
+    echo $f
+    echo ${f#./map}
+done
+
+## Execute post-processing
 
 rm -f ./deploynow
